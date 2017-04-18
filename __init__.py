99a292c70766fb866c94bcea4727c2f4ff94d3f1
Bedartha Goswami <bedartha@gmail.com>
2017-04-18 16:11:17 +0200

    added init file

---
+++ __init__.py
@@ -0,0 +1,40 @@
+"""
+Package mkt.
+
+Python module to compute the Mann-Kendall test for trend in time series data.
+
+This module contains a single function 'test' which implements the Mann-Kendall
+test for a linear trend in a given time series. 
+
+The latest version of this package is available at
+https://github.com/UP-RS-ESP/mkt
+
+For detailed documentation, please visit
+http://up-rs-esp.github.io/mkt
+
+"""
+# Created: Tue Apr 18, 2017  04:10PM
+# Last modified: Tue Apr 18, 2017  04:10PM
+# Copyright: Bedartha Goswami <goswami@uni-potsdam.de>
+
+
+__name__ = "mkt"
+"""The project name."""
+
+__author__ = "Bedartha Goswami <goswami@uni-potsdam.de>"
+"""The primary author."""
+
+__copyright__ = "Copyright (c) 2016 Bedartha Goswami."
+"""The copyright holder of this package 'bpl'."""
+
+__license__ = "GNU General Public License v2"
+"""The license governing the use and distribution of this module."""
+
+__url__ = "https://github.com/UP-RS-ESP/mkt"
+"""The URL for the package's homepage."""
+
+__version__ = 1.0.1
+"""Version number."""
+
+__date__ = "2017-04-18"
+"""The release date of this version."""
